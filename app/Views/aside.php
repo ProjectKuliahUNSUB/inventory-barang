0790c84b08b8b1959f60d9f103dded195f76e429
bootleg <egiesugina704@gmail.com>
2023-11-26 21:17:56 +0700

    add

--- app/Views/aside.php
+++ app/Views/aside.php
@@ -18,7 +18,7 @@
   <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
       <li class="nav-item">
-        <a href="/dashboard" class="nav-link">
+        <a  href="<?= base_url('dashboard'); ?>" class="nav-link">
           <i class="nav-icon fas  fa-solid fa-chart-line"></i>
 
           <p>
